6c72343df4240bd191a8b74cc5636db592b8f502
Affan Hasan <affanhasan88@gmail.com>
2017-03-16 21:58:30 +0500

    Modifications in EntryPoint.java

--- app/app/src/main/java/app/EntryPoint.java
+++ app/app/src/main/java/app/EntryPoint.java
@@ -5,8 +5,13 @@
  */
 package app;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -36,8 +41,8 @@ public class EntryPoint {
         }
 //        Creating File commands.json
         Path commands_json_file_path = Paths.get("app-interface/commands.json");
-        File commcommands_json_file = new File(commands_json_file_path.toString());
-        if (!commcommands_json_file.exists()) {
+        File commands_json_file = new File(commands_json_file_path.toString());
+        if (!commands_json_file.exists()) {
             try {
                 Files.createFile(commands_json_file_path);
                 System.out.println("File \'app-interface\\commands.json\' created");
@@ -46,6 +51,24 @@ public class EntryPoint {
             }
         } else {
             System.out.println("File \'app-interface\\commands.json\' already exists");
+        }
+
+        try (FileWriter fw = new FileWriter(commands_json_file)) {
+            InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("templates/commands_template.json");
+            int character = 0;
+            char abc;
+            String fileContent = null;
+            while ((character = is.read()) != -1) {
+                abc = (char) character;
+                fileContent += abc;
+            }
+            System.out.println(fileContent);
+//            System.out.println(EntryPoint.class.getClassLoader().getResource("templates/commands_template.json").getFile());
+//            File temp_file = new File(EntryPoint.class.getClassLoader().getResource("templates/commands_template.json").getFile());
+//            FileReader fr =new FileReader(temp_file);
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
         }
     }
 
